12b9307006e71e4dd29c3c7daad1aa54cfcf7539
subhajitmajumder90 <subhajit.mazumder@viavitae.co.in>
2019-12-17 18:56:56 +0530

    pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
+import { TestpipePipe } from './testpipe.pipe';
+ 
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    HomeComponent
+    HomeComponent,
+    TestpipePipe
   ],
   imports: [
     BrowserModule,
