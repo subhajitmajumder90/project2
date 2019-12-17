b34a5560f9599cd63df122bfa2cca6506e412a7b
subhajitmajumder90 <subhajit.mazumder@viavitae.co.in>
2019-12-17 12:33:35 +0530

    added bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path:"",
+    component:HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
